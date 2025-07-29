a5b7412f0ccc80501cef62f7ccfcb8c15ade45f1
Alsu S <alsitd38@gmail.com>
2025-07-29 23:47:57 +0300

    Homework 5 corrected

--- homework/alsu_sitdikova/Homework_5/task3.py
+++ homework/alsu_sitdikova/Homework_5/task3.py
@@ -8,10 +8,4 @@ Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 
-print(f"Students {students[0]}, {students[1]}, {students[2]} study these "
-      f"subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}")
-
-print("Students {}, {}, {} study these subjects: {}, {}, {}".format(*students, *subjects))
-
-print("Students %s, %s, %s study these subjects: %s, %s, %s" % (students[0], students[1], students[2],
-                                                                subjects[0], subjects[1], subjects[2]))
+print("Students {} study these subjects: {}".format(', '.join(students), ', '.join(subjects)))
