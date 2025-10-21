58a33ba834cfb78d1bb70af53a304c37e33f987e
Alsu S <alsitd38@gmail.com>
2025-10-21 22:55:42 +0300

    Homework 10 done

---
+++ homework/alsu_sitdikova/Homework_10/task1.py
@@ -0,0 +1,30 @@
+"""Задание на декораторы 1
+Создайте универсальный декоратор, который можно будет применить к любой функции. Декоратор должен делать следующее:
+он должен распечатывать слово "finished"после выполнения декорированной функции.
+Код, использующий этот декоратор может выглядеть, например, так:
+
+@finish_me
+def example(text):
+    print(text)
+example('print me')
+
+В результате работы будет такое:
+print me
+finished
+"""
+
+
+def finish_me(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print("finished")
+        return func
+    return wrapper
+
+
+@finish_me
+def example(text):
+    print(text)
+
+
+example('print me')
